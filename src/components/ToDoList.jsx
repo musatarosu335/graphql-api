10bc4c4e36b86efcf5d954a562b7ece6afe8ba33
Kentaro Sakae <musamusa0607@gmail.com>
2018-05-06 15:15:33 +0900

    MutationでToDoを追加

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import gql from 'graphql-tag';
-import { graphql } from 'react-apollo';
+import { graphql, compose } from 'react-apollo';
 
 // Queryを定義
 const TODOS_QUERY = gql`
@@ -13,23 +13,74 @@ const TODOS_QUERY = gql`
   }
 `;
 
-const ToDoList = (props) => {
-  if (props.data && props.data.loading) {
-    return (
-      <div>Now loading...</div>
-    );
+// Mutationを定義
+const ADD_MUTATION = gql`
+  mutation AddMutation($input: ToDoInput) {
+    addToDo(input: $input) {
+      title
+    }
+  }
+`;
+
+class ToDoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+    };
   }
 
-  // Queryで所得したデータの初期化
-  const todoList = props.data.todos;
+  addToDo() {
+    // Mutationを実行
+    this.props.addMutation({
+      variables: {
+        input: {
+          title: this.state.title,
+        },
+      },
+      refetchQueries: [{ query: TODOS_QUERY }],
+    }).then(({ data }) => console.log(data.addToDo));
+    this.setState({
+      title: '',
+    });
+  }
 
-  return (
-    <div>
-      {todoList.map(todo => (
-        <p key={todo.key}>{todo.title}</p>
-      ))}
-    </div>
-  );
+  render() {
+    if (this.props.data && this.props.data.loading) {
+      return (
+        <div>Now loading...</div>
+      );
+    }
+
+    // Queryで所得したデータの初期化
+    const todoList = this.props.data.todos;
+
+    return (
+      <div>
+        <input
+          type="text"
+          value={this.state.title}
+          onChange={e => this.setState({ title: e.target.value })}
+        />
+        <button
+          type="submit"
+          onClick={() => this.addToDo()}
+        >
+          追加
+        </button>
+        {todoList.map(todo => (
+          <p key={todo.id}>{todo.title}</p>
+        ))}
+      </div>
+    );
+  }
+}
+
+ToDoList.propTypes = {
+  data: PropTypes.object.isRequired,
 };
 
-export default graphql(TODOS_QUERY)(ToDoList);
+export default compose(
+  graphql(TODOS_QUERY),
+  graphql(ADD_MUTATION, { name: 'addMutation' }),
+)(ToDoList);
