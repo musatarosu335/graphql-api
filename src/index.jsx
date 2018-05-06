bbe4972175a3dc2447eed59634f61dc12b769dc1
Kentaro Sakae <musamusa0607@gmail.com>
2018-05-06 14:09:35 +0900

    GraphQL APIとの接続

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { ApolloProvider } from 'react-apollo';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
 import App from './components/App';
 
+// HttpLinkインスタンスを生成
+const httpLink = new HttpLink({
+  uri: 'http://localhost:4000/graphql',
+});
+
+// ApolloClientインスタンスを生成
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+// ApolloProviderでラップ
+const ApolloApp = () => (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
+
 ReactDom.render(
-  <App />,
+  <ApolloApp />,
   document.getElementById('root'),
 );
